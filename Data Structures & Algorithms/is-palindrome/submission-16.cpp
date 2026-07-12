c10885b3e3e26cce8aa6ba7fc665683d8ec3da4e
S-Vikram2008 <lightvikram@gmail.com>
2026-07-12 20:22:33 +0530

    Add: is-palindrome - submission-16

---
+++ Data Structures & Algorithms/is-palindrome/submission-16.cpp
@@ -0,0 +1,30 @@
+
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        #include <cctype>
+        int left=0;
+        int right=s.length()-1;
+
+        while (left<right){
+            if (not (isalnum(s[left]))){
+                left+=1;
+            }
+            else if (not (isalnum(s[right]))){
+                right-=1;
+            }
+            else if (tolower(s[left])!=tolower(s[right])){
+                return false;
+            }
+            else{
+                left+=1;
+                right-=1;
+            }
+        }
+       
+        return true;
+        
+        
+        
+    }
+};
