bc07fab5b02f8a8b339fcec2beffeb4f67c55005
S_Vikram2008 <lightvikram@gmail.com>
2026-07-13 13:50:24 +0530

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.py
@@ -0,0 +1,12 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        hashmap=dict()
+        for i in strs:
+            key=''.join(sorted(i))
+            if key not in hashmap:
+                hashmap[key]=[i]
+            else:
+                hashmap[key].append(i)
+        
+        return list(hashmap.values())
+        
